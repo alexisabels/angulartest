4a6d6975d55695b92c64b7b90478139c16bed047
alexisabels <isabelsanchezalex@gmail.com>
2025-01-03 15:23:12 +0100

    counter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { GreetingsComponent } from './greetings/greetings.component';
 import { OpinionComponent } from './opinion/opinion.component';
 import { TodoComponent } from './todo/todo.component';
+import { CounterComponent } from './counter/counter.component';
 
 export const routes: Routes = [
   { path: 'greetings', component: GreetingsComponent },
   { path: 'opinion', component: OpinionComponent },
   { path: 'todo', component: TodoComponent },
+  { path: 'counter', component: CounterComponent },
 ];
